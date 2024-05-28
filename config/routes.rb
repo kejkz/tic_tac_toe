58808c0815021d0ece76384239c48709dff76aa7
Vladimir Karan Grahovac <kejkzz@gmail.com>
2024-05-28 19:39:50 +0200

    Setup redis and a bit of play

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :games, only: [:new, :show, :create, :update]
+  resources :users, only: [:new, :create]
+
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#new"
 end
